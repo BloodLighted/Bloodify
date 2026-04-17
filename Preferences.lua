7da77f43f9b23884646584969d6b6f01d0a3062c
BloodLight <braymanthetea5@gmail.com>
2026-04-17 12:55:22 -0500

    Updated Preferences.lua
    Updated to V1.2.0

--- Preferences.lua
+++ Preferences.lua
@@ -1,11 +1,11 @@
---// > BloodifyPreferences.lua < //--
---// ? all the settings used across the Bloodify plugin
---// ? select something that isn't the module to delete the module and save your preferences
---// ? don't worry if you delete some preferences in here, there are fallbacks for most, if not all of them
+--// > ConvertPlusPreferences.lua < //--
+--// all the settings used across the ConvertPlus plugin
+--// select something that isn't the module to delete the module and save your preferences
+--// don't worry if you delete some preferences in here, there are fallbacks for most, if not all of them
 
 return {
 	--// > NotificationService.lua < //--
-	--// ? all the settings for the Notifications UI
+	--// all the settings for the Notifications UI
 	--// ! general settings
 	notifPadding = 10, --// (num) | the distance in pixels between each notification (default: 10)
 	maxNotificationWidth = 1200, --// (num) | the maximum width of the notifications (default: 1200)
@@ -16,7 +16,7 @@ return {
 	floatSpeed = 1, --// (num) | the multiplier of the wiggle speed on the text (default: 1)
 	--// ! hover settings
 	hoverRadius = 200, --// (num) | the radius of the mouse's hover area (default: 200)
-	hoverMovePower = 8, --// (num) | the max amount of pixels a letter will move when hovered (default: 20)
+	hoverMovePower = 8, --// (num) | the max amount of pixels a letter will move when hovered (default: 8)
 	hoverScaleLimit = 1.15, --// (num) | the max scale of a letter when hovered (default: 1.15)
 	bounceElasticity = 0.1, --// (num) | the elasticity of the bounce-back when unhovered (default: 0.1)
 	bounceFriction = 0.85, --// (num) | the friction of the bounce-back when unhovered (default: 0.85)
@@ -37,7 +37,7 @@ return {
 	strokeColor = Color3.fromRGB(0, 0, 0), --// (Color3) | the color of the stroke on the text (default: Color3.fromRGB(0, 0, 0))
 
 	--// > AccessoryService.lua < //--
-	--// ? all the settings for the Accessory Converter
+	--// all the settings for the Accessory Converter
 	--// ! weld settings
 	useLegacyWeldParenting = false, --// (bool) | if true, will put the welds in a folder under Accessories/Hats instead of the accessory (default: false)
 	useLegacyAccessoryWeld = false, --// (bool) | if true, the weld will be a normal Weld instead of a WeldConstraint. this means you cannot move the part or the weld breaks (default: false)
@@ -52,15 +52,10 @@ return {
 	accessoryFluidForces = false, --// (bool) | if true, keeps EnableFluidForces enabled. i don't even know what this does so just don't touch it ok? ok (default: false)
 
 	--// > FaceService.lua < //--
-	--// ? all the settings for the Face/Head Converter
+	--// all the settings for the Face/Head Converter
 	facePartName = "FacePart", --// (string) | the name that the facePart will use (default: "FacePart")
 	faceMeshName = "HeadMesh", --// (string) | the name that the Specialmesh under FacePart will use (default: "HeadMesh")
 	faceDecalName = "MainFaceDecal", --// (string) | the name that the face Decal under facePart will use (default: "MainFaceDecal")
 	useLegacyFaceWeld = false, --// (bool) | if true, the weld will be a normal Weld instead of a WeldConstraint. this means you cannot move the part or the weld breaks (default: false)
 	keepOldFace = false, --// (bool) | if true, the original face after the conversion will stay under a folder of the head. this is what HDify does also (default: false)
-	
-	--// > OutlineService.lua < //--
-	--// ? all the settings for the extremely buggy Outline creator
-	outlineScaleMultiplier = -1.075, --// (num) | the scale multiplier that is applied to the outline (default: -1.075)
-	outlineColor = Color3.fromRGB(0, 0, 0), --// (Color3) | the color that is applied to the outline (default: Color3.fromRGB(0, 0, 0))
 }
